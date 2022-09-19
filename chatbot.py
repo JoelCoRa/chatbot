86b7cab1f6d9b85892e357a9bff30e47dab9ae52
Joel Colin <joelcolinr@gmail.com>
2022-09-18 23:07:59 -0500

    Fase 2

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ import random
 import re
 
 def getResponse(user_input):
-    split_message = re.split(r'\s|[,:;.?!-_]\s*',user_input.lower())
+    split_message = re.split(r'\s|[,:;.?!-_*+]\s*',user_input.lower())
     response = check_all_messages(split_message)
     return response
 
@@ -34,17 +34,17 @@ def check_all_messages(message):
         nonlocal highest_prob
         highest_prob[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
-
-
     # Saludos y Despedida
-    response('Hola', ['hola','alo','saludos','wenas'], single_response=True)
-    response('Estoy bien y tu?', ['como','estas','te','va','vas','encuentras','sientes'], required_words=['como'])
-    # response('Estamos ubicados en la calle 23 no.123', ['ubicados','direccion','donde','ubicacion'], single_response=True)
-    response('Siempre a la orden', ['gracias','te lo agradezco','tenkiu','thx'], single_response=True)
-
+    response('Buen dia, en que puedo apoyarlo el dia de hoy?', ['hola','alo','saludos','wenas','buen','dia'], single_response=True)
+    response('Me encuentro bien y usted?', ['como','estas','te','va','vas','encuentras','sientes'], required_words=['como','sientes'])
+    response('Me alegro que se encuentre bien',['bien','me','gracias','siento'],required_words=['bien'])
+    response('Oh lamento escucharlo, hay algo en lo que pueda apoyarle?',['siento','mal','me'],required_words=['mal'])      
+    response('Me llamo '+ nombreBot , ['como','te','llamas','cual','es','tu','nombre'],required_words=['llamas'])
+    
+    response('Siempre a la orden. Si desea terminar la conversación presione Ctrl + C', ['gracias','te lo agradezco','tenkiu','thx','salir'], single_response=True)
 
     #1. ¿Que son las artesanias?
-    response('Las artesanías mexicanas son resultado del trabajo de un artesano. Cada una es característica de cada región','Que es una artesania mexicana',['que','es','una','artesania','mexicana'], required_words=['que','es','artesania'])
+    response('Las artesanías mexicanas son resultado del trabajo de un artesano. Cada una es característica de cada región','Que es una artesania mexicana',['artesania','mexicana'], required_words=['artesania'])
 
     #2. ¿De que estan hechas las artesanias? 
     response('Son elaboradas, regularmente, con materiales de origen natural como la madera, el barro, las telas, las semillas, piedras y metales',['de','que','estan','hechas','compuestas','elaborar'],required_words=['hechas'])
@@ -74,7 +74,7 @@ def check_all_messages(message):
 
 
     #8. ¿Qué es un sarape?
-    response('El sarape o jorongo es una prenda única, llena de pasión y cultura que ha acompañado a los mexicanos desde tiempos coloniales y ha sido utilizada por campesinos, hacendados, artesanos, charros, revolucionarios, generales e incluso artistas a lo largo de la historia.',['que','es'],required_words=['sarape','zarape','jorongo'])
+    response('El sarape o jorongo es una prenda única, llena de pasión y cultura que ha acompañado a los mexicanos desde tiempos coloniales y ha sido utilizada por campesinos, hacendados, artesanos, charros, revolucionarios, generales e incluso artistas a lo largo de la historia.',['que','es','jorongo','zarape'],required_words=['sarape','zarape'])
     #8.2 ¿De donde proviene el sarape?
     response('El sarape proviene de Saltillo, Coahuila, aunque cada estado tiene su propio estilo, a veces cambiando la forma de elaboración, el diseño, etc.', ['de','donde','es','proviene','provienen','viene','vienen'], required_words=['sarape','zarape','jorongo'])
     
@@ -85,7 +85,7 @@ def check_all_messages(message):
 
     #10. ¿Qué es un alebrije?
     response('Los alebrijes son seres imaginarios conformados por elementos fisonómicos de animales diferentes, una combinación de varios animales, no solo fantásticos sino también reales que forman un ser fantástico. Estos seres se representan como artesanías fabricadas con cartón o papel, y estructura de alambre, o con madera, que se pintan con colores mayormente alegres y vibrantes',['que','es'],required_words=['alebrije'])
-    #8.2 ¿De donde provienen los alebrijes?
+    #10.2 ¿De donde provienen los alebrijes?
     response('Los alebrijes son originarios de la Ciudad de México más concretamente del barrio de la Merced. Sin embargo, actualmente su elaboración es más popular en el estado de Oaxaca',['de','donde','es','proviene','provienen','viene','vienen'],required_words=['alebrije'])
 
     #11. ¿La guayabera es una artesanía mexicana?
@@ -93,10 +93,6 @@ def check_all_messages(message):
     
     #12. ¿Cuáles son los juguetes que se fabrican de forma artesanal?
     response('Son varios, algunos ejemplos son el trompo, la pirinola, la matraca, el balero o la divertida lotería, además de coches y camiones de madera o un sinfín de muñecas entre las que destacan las típicas de trenzas y colores vivos',['cuales','son','juguetes','artesanal'],required_words=['juguetes','fabrican'])
-    #13. ¿Que regiones de México, realizan estas obras?
-
-
-
 
     best_match = max(highest_prob, key=highest_prob.get)
     # print(highest_prob)
@@ -107,11 +103,24 @@ def check_all_messages(message):
         return best_match
 
 def unknown():
-    response = ['puedes decirlo de nuevo?','No estoy seguro de lo que quieres', 'buscalo en google a ver que tal xd'][random.randrange(3)]
+    response = ['Puedes decirlo de nuevo?','No estoy seguro de lo que quieres', 'No tengo ese conocimiento aún, lo siento :c','No te he entendido','No hablo extranjero jeje'][random.randrange(5)]
     return response
 
 
 
 if __name__ == '__main__':
+    nombreBot = 'Bot'
+    nombreUsuario = 'Usuario'
+    inicio = 'Bienvenido a su asistente de Artesanías Mexicanas'
+    print(nombreBot + ": " + inicio)
+    opcion = input(nombreBot + ": " +'Mi nombre es ' + nombreBot + ', le gustaría decirme el suyo? ')
+
+    if(opcion == 'S' or opcion == 's' or opcion == 'Y' or opcion == 'y'):
+        nombreUsuario = input('Escriba su nombre: ')
+        print(nombreBot + ': Bienvenido ' + nombreUsuario + "!!!")
+    else:
+        print(nombreBot + ': Bien lo llamaré ' + nombreUsuario)
+
+                
     while True:
-        print("Bot: " + getResponse(input('You: ')))
+        print(nombreBot + ": " + getResponse(input(nombreUsuario + ': ')))
